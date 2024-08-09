ca20a099221bf50f8b2a38f86aed2034ad897718
Lomonosov-Dmitry <cooler7@icloud.com>
2024-08-09 17:32:54 +0300

    Убрал лишний HashSet. Переделал методы связанного списка.

--- src/model/HistoryLinkedList.java
+++ src/model/HistoryLinkedList.java
@@ -2,77 +2,45 @@ package model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class HistoryLinkedList {
-    public Node<Task> head;
     public Node<Task> tail;
-    public int size = 0;
-    private final HashSet<Node<Task>> historySet;
     private final HashMap<Integer, Node<Task>> historyMap;
 
     public HistoryLinkedList() {
-        this.head = null;
         this.tail = null;
-        historySet = new HashSet<>();
         historyMap = new HashMap<>();
     }
 
     public <T extends Task> void linkLast(T task) {
-        if (head == null) {
-            Node<Task> newNode = new Node<>(task);
-            historySet.add(newNode);
-            head = newNode;
-            tail = newNode;
-            historyMap.put(task.getId(), newNode);
-        } else if (head.data.equals(task) && historySet.size() == 1) {
-            head.data = task;
-            return;
-        } else {
-            if (historyMap.containsKey(task.getId()))
-                removeNode(historyMap.get(task.getId()));
-            Node<Task> newNode = new Node<>(task);
+        if (historyMap.containsKey(task.getId()))
+            removeNode(historyMap.get(task.getId()));
+        Node<Task> newNode = new Node<>(task);
+        if (tail != null) {
             newNode.prev = tail;
             tail.next = newNode;
-            tail = newNode;
-            historySet.add(newNode);
-            historyMap.put(task.getId(), newNode);
         }
-        size++;
-
+        tail = newNode;
+        historyMap.put(task.getId(), newNode);
     }
 
     public void removeTaskById(int id) {
         if (historyMap.containsKey(id)) {
             removeNode(historyMap.get(id));
-            historyMap.remove(id);
         }
     }
 
     private void removeNode(Node<Task> node) {
         if (node != null) {
             historyMap.remove(node.data.getId());
-            if (node.prev == null && node.next == null) {
-                historySet.clear();
-                head = null;
-                tail = null;
+            if (node.next == null) {
+                tail = node.prev;
+                node.prev.next = null;
+            } else if (node.prev == null) {
+                node.next.prev = null;
             } else {
-                if (node.prev == null) {
-                    Node<Task> nextNode = node.next;
-                    nextNode.prev = null;
-                    head = nextNode;
-                } else if (node.next == null) {
-                    Node<Task> privNode = node.prev;
-                    privNode.next = null;
-                    tail = privNode;
-                } else {
-                    Node<Task> privNode = node.prev;
-                    Node<Task> nextNode = node.next;
-                    privNode.next = nextNode;
-                    nextNode.prev = privNode;
-                }
-                historySet.remove(node);
-                size--;
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
             }
         }
     }
@@ -81,11 +49,10 @@ public class HistoryLinkedList {
         ArrayList<Task> tasks = new ArrayList<>();
         if (tail != null) {
             Node<Task> node = tail;
-            while (node.prev != null) {
+            while (node != null) {
                 tasks.add(node.data);
                 node = node.prev;
             }
-            tasks.add(node.data);
         }
         return tasks;
     }
